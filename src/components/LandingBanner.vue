c29df531cc1157e57f957cab18c91ffa05d0f7aa
Jordy Backes <backes.jordy@gmail.com>
2019-07-07 11:56:31 +0200

    added responsiveness for banner

--- src/components/LandingBanner.vue
+++ src/components/LandingBanner.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
-    <img src="../assets/images/riverspriteEyeShadow.png" alt="" class="banner-logo" />
-    <h1 class="title text-white font-weight-bold">TFT Cheat Sheets</h1>
-    <h3 class="text-white">
-      Find team compositions based on your current in-game champions
-    </h3>
+    <mq-layout mq="lg+">
+      <img
+        src="../assets/images/riverspriteEyeShadow.png"
+        alt=""
+        class="banner-logo"
+      />
+      <h1 class="title text-white font-weight-bold">TFT Cheat Sheets</h1>
+      <h3 class="text-white">
+        Find team compositions based on your current in-game champions
+      </h3> </mq-layout
+    >
+    <mq-layout :mq="['sm', 'md']">
+      <img
+        src="../assets/images/riverspriteEyeShadow.png"
+        alt=""
+        class="sm-md-banner-logo"
+      />
+      <h1 class="sm-md-title text-white font-weight-bold">TFT Cheat Sheets</h1>
+      <h6 class="text-white">
+        Find team compositions based on your current in-game champions
+      </h6>
+    </mq-layout>
   </div>
 </template>
 
@@ -17,6 +34,12 @@ export default {};
   font-size: 52px;
 }
 .banner-logo {
-    height: 300px;
+  height: 300px;
+}
+.sm-md-title {
+  font-size: 32px;
+}
+.sm-md-banner-logo {
+  height: 150px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -6,8 +6,19 @@ import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueMq from 'vue-mq'
 
 Vue.use(BootstrapVue)
+Vue.use(VueMq, {
+  breakpoints: { // default breakpoints - customize this
+    sm: 576,
+    md: 768,
+    lg: 992,
+    xl: 1200,
+    xxl: 99999
+  },
+  defaultBreakpoint: 'sm' // customize this for SSR
+});
 
 Vue.config.productionTip = false
 
